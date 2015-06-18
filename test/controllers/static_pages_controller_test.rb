b4323744ab2dd1301ce0d9efdd1d8060b9feb1f2
shin-ruby <shin.php@gmail.com>
2015-06-18 15:24:15 +0800

    Finish layout

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,15 +1,20 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+
+	def setup
+		@base_title = "Writings Hub"
+	end
+
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Writings Hub"
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | Writings Hub"
+    assert_select "title", "Help | #{@base_title}"
   end
 end
